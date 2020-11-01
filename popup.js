333fb598dbe641b31b3725b365a1163de67d65b2
yolanpibrac <yolan.pibrac@gmail.com>
2020-11-02 00:19:09 +0100

    feature(contentScript): set color directly with popup

--- popup.js
+++ popup.js
@@ -15,10 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
             editImages(event.target.value)
         }
     }
-    colorPicker.addEventListener("change", watchColorPicker, false);
+    colorPicker.addEventListener("input", watchColorPicker, false);
 });
-
-
 
 
 const shadeColor = (color, percent) => {
@@ -43,16 +41,23 @@ const shadeColor = (color, percent) => {
 }
 
 
- const editImages = (inputColor) => {
-    const imagesOfWebsite = document.getElementsByTagName("img")
-    for (imageHtml of imagesOfWebsite) { 
-        const backgroundImage = window.getComputedStyle(imageHtml).background
-        if(backgroundImage.includes("linear-gradient(45deg, rgb(239, 239, 239) 25%, rgba(239, 239, 239, 0) 25%, rgba(239, 239, 239, 0) 75%, rgb(239, 239, 239) 75%, rgb(239, 239, 239)) repeat scroll 0px 0px / 21px 21px padding-box border-box, rgb(255, 255, 255) linear-gradient(45deg, rgb(239, 239, 239) 25%, rgba(239, 239, 239, 0) 25%, rgba(239, 239, 239, 0) 75%, rgb(239, 239, 239) 75%, rgb(239, 239, 239))")){
-            const color = shadeColor(inputColor,100)
-            imageHtml.style.background = `linear-gradient(45deg,${color} 25%,rgba(239,239,239,0) 25%,rgba(239,239,239,0) 75%,${color} 75%,${color}),linear-gradient(45deg,${color} 25%,white 25%,white 75%,${color} 75%,${color})`
-            imageHtml.style.backgroundPosition = "0 0,10px 10px"
-            imageHtml.style.backgroundSize = "21px 21px"
-        }
 
-    }
+const editImages = (inputColor) => {
+    let imagesOfWebsite = []
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code: `
+            imagesOfWebsite = document.getElementsByTagName("img");
+            for (imageHtml of imagesOfWebsite) { 
+                const backgroundImage = window.getComputedStyle(imageHtml).background;
+                if(backgroundImage.includes("linear-gradient(45deg") && backgroundImage.includes("21px 21px")){
+                    imageHtml.style.background = "linear-gradient(45deg,${inputColor} 25%,rgba(239,239,239,0) 25%,rgba(239,239,239,0) 75%,${inputColor} 75%,${inputColor}) repeat scroll 0px 0px / 21px 21px padding-box border-box, rgb(255, 255, 255) linear-gradient(45deg,${inputColor} 25%,white 25%,white 75%,${inputColor} 75%,${inputColor})";
+                    imageHtml.style.backgroundPosition = "0 0,10px 10px";
+                    imageHtml.style.backgroundSize = "21px 21px";
+                }
+            }
+            `}
+        );
+    });
 }
