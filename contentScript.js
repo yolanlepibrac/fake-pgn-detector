fc26c04d1daea15b4b04911e3af36847a7e9ad98
yolanpibrac <yolan.pibrac@gmail.com>
2020-12-19 12:12:39 +0100

    feature(contentScript): apply background on all images

--- contentScript.js
+++ contentScript.js
@@ -4,11 +4,11 @@ const editImages = async (color) => {
     const imagesOfWebsite = document.getElementsByTagName("img")
     for (imageHtml of imagesOfWebsite) { 
         const backgroundImage = window.getComputedStyle(imageHtml).background
-        if(backgroundImage.includes("linear-gradient(45deg, rgb(239, 239, 239) 25%, rgba(239, 239, 239, 0) 25%, rgba(239, 239, 239, 0) 75%, rgb(239, 239, 239) 75%, rgb(239, 239, 239)) repeat scroll 0px 0px / 21px 21px padding-box border-box, rgb(255, 255, 255) linear-gradient(45deg, rgb(239, 239, 239) 25%, rgba(239, 239, 239, 0) 25%, rgba(239, 239, 239, 0) 75%, rgb(239, 239, 239) 75%, rgb(239, 239, 239))")){
+        // if(backgroundImage.includes("linear-gradient(45deg, rgb(239, 239, 239) 25%, rgba(239, 239, 239, 0) 25%, rgba(239, 239, 239, 0) 75%, rgb(239, 239, 239) 75%, rgb(239, 239, 239)) repeat scroll 0px 0px / 21px 21px padding-box border-box, rgb(255, 255, 255) linear-gradient(45deg, rgb(239, 239, 239) 25%, rgba(239, 239, 239, 0) 25%, rgba(239, 239, 239, 0) 75%, rgb(239, 239, 239) 75%, rgb(239, 239, 239))")){
             imageHtml.style.background = `linear-gradient(45deg,${color} 25%,rgba(239,239,239,0) 25%,rgba(239,239,239,0) 75%,${color} 75%,${color}) repeat scroll 0px 0px / 21px 21px padding-box border-box, rgb(255, 255, 255) linear-gradient(45deg,${color} 25%,white 25%,white 75%,${color} 75%,${color})`
             imageHtml.style.backgroundPosition = "0 0,10px 10px"
             imageHtml.style.backgroundSize = "21px 21px"
-        }
+        // }
 
     }
 }
