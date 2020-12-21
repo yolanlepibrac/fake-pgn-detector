6aa2b1857e9e3602a6b799ad27c231486d76f9ab
yolanpibrac <yolan.pibrac@gmail.com>
2020-12-21 12:38:42 +0100

    feature(app): refactor to apply color only on quadrillé images or when checkbox applyEverywhere is checked

--- contentScript.js
+++ contentScript.js
@@ -1,29 +1,35 @@
 // ContentScript run in a page context (defined in manifest)
+// google background : "linear-gradient(45deg, rgb(239, 239, 239) 25%, rgba(239, 239, 239, 0) 25%, rgba(239, 239, 239, 0) 75%, rgb(239, 239, 239) 75%, rgb(239, 239, 239)) repeat scroll 0px 0px / 21px 21px padding-box border-box, rgb(255, 255, 255) linear-gradient(45deg, rgb(239, 239, 239) 25%, rgba(239, 239, 239, 0) 25%, rgba(239, 239, 239, 0) 75%, rgb(239, 239, 239) 75%, rgb(239, 239, 239))"
 
-const editImages = (color) => {
+const hasGoogleGradientColor = (backgroundImage) => {
+    const backgroundImageTrim = backgroundImage.replace(/\s/g, '');
+    if(backgroundImageTrim.includes("21px21px")){
+        return true
+    }
+    return false
+}
+
+const editImages = (color, activeEverywhere) => {
     const imagesOfWebsite = document.getElementsByTagName("img")
     for (imageHtml of imagesOfWebsite) { 
         const backgroundImage = window.getComputedStyle(imageHtml).background
-        // if(backgroundImage.includes("linear-gradient(45deg, rgb(239, 239, 239) 25%, rgba(239, 239, 239, 0) 25%, rgba(239, 239, 239, 0) 75%, rgb(239, 239, 239) 75%, rgb(239, 239, 239)) repeat scroll 0px 0px / 21px 21px padding-box border-box, rgb(255, 255, 255) linear-gradient(45deg, rgb(239, 239, 239) 25%, rgba(239, 239, 239, 0) 25%, rgba(239, 239, 239, 0) 75%, rgb(239, 239, 239) 75%, rgb(239, 239, 239))")){
-            imageHtml.style.background = `linear-gradient(45deg,${color} 25%,rgba(239,239,239,0) 25%,rgba(239,239,239,0) 75%,${color} 75%,${color}) repeat scroll 0px 0px / 21px 21px padding-box border-box, rgb(255, 255, 255) linear-gradient(45deg,${color} 25%,white 25%,white 75%,${color} 75%,${color})`
-            imageHtml.style.backgroundPosition = "0 0,10px 10px"
-            imageHtml.style.backgroundSize = "21px 21px"
-        // }
-
+        if(hasGoogleGradientColor(backgroundImage) || activeEverywhere){
+            setBackgroundGradientToImage(imageHtml, color)
+        }
     }
+}
+
+const setBackgroundGradientToImage = (imageHtml, color) => {
+    imageHtml.style.background = `linear-gradient(45deg,${color} 25%,rgba(239,239,239,0) 25%,rgba(239,239,239,0) 75%,${color} 75%,${color}) repeat scroll 0px 0px / 21px 21px padding-box border-box, rgb(255, 255, 255) linear-gradient(45deg,${color} 25%,white 25%,white 75%,${color} 75%,${color})`
+    imageHtml.style.backgroundPosition = "0 0,10px 10px"
+    imageHtml.style.backgroundSize = "21px 21px"
 }
 
 const getCurrentColorAndSetToPng = async () => {
     const storage  = await getCurrentStorage();
     const color = storage.fakePngDetectorColor
-    const active = storage.fakePngDetectorActiveEverywhere
-    if(active){
-        editImages(color)
-    }
-}
-
-const setTransparentColorToPng = () => {
-    editImages("rgba(0,0,0,0)")
+    const activeEverywhere = storage.fakePngDetectorActiveEverywhere
+    editImages(color, activeEverywhere)
 }
 
 getCurrentColorAndSetToPng()
--- popup.js
+++ popup.js
@@ -1,22 +1,44 @@
 // Popup control the menu opened when click on extension icon
 
-const initializedPopupColorPicker = async () => {
-        document.addEventListener('DOMContentLoaded', async () => {
-        let colorPicker = document.getElementById('fakePngDetector-colorPicker');
-        function handleChangeColor(event) {
-            if(event.target){
-                setColorToStorage(event.target.value)
-                setCurrentColorToPng()
-            }
-        }
-        colorPicker.addEventListener("input", handleChangeColor, false);
+const handleChangeColor = (event) => {
+    if(event.target){
+        setColorToStorage(event.target.value)
+        relaunchContentScript()
+    }
+}
+
+const handleChangeCheckbox = (event) => {
+    setActiveToStorage(event.target.checked)
+    if(event.target.checked){
+        relaunchContentScript()
+    }else {
+        refreshTab()
+    }
+}
+
+const refreshTab = () => {
+    chrome.tabs.getSelected(null, (tab) => {
+        var code = 'window.location.reload();';
+        chrome.tabs.executeScript(tab.id, {code: code});
+    })
+}
+const initializedPopup = async () => {
+    
+    document.addEventListener('DOMContentLoaded', async () => {
         const currentStorage = await getCurrentStorage()
+        const colorPicker = document.getElementById('fakePngDetector-colorPicker');
+        const checkboxActive = document.getElementById('fakePngDetector-activeEverywhere')
+        
+        colorPicker.addEventListener("input", handleChangeColor, false);
         colorPicker.value = currentStorage.fakePngDetectorColor
+
+        checkboxActive.checked = currentStorage.fakePngDetectorActiveEverywhere
+        checkboxActive.addEventListener('change', handleChangeCheckbox, false)
     });
 }
 
 
-const setCurrentColorToPng = () => {
+const relaunchContentScript = () => {
     chrome.tabs.query({}, (tabs) => {
         tabs.map((_tab, index) => {
             chrome.tabs.executeScript(tabs[index].id, {code: `getCurrentColorAndSetToPng()`});
@@ -24,27 +46,4 @@ const setCurrentColorToPng = () => {
     });
 }
 
-const setColorToPng = () => {
-    chrome.tabs.query({}, (tabs) => {
-        tabs.map((_tab, index) => {
-            chrome.tabs.executeScript(tabs[index].id, {code: `setTransparentColorToPng()`});
-        })
-    });
-}
-
-document.addEventListener('DOMContentLoaded', async () =>  {
-    const currentStorage = await getCurrentStorage()
-    const checkboxActive = document.getElementById('active')
-    checkboxActive.checked = currentStorage.fakePngDetectorActiveEverywhere
-    checkboxActive.addEventListener('change', (event) => {
-        if(event.target.checked){
-            setCurrentColorToPng()
-        }else {
-            setColorToPng()
-        }
-        setActiveToStorage(event.target.checked)
-      })
-});
-
-
-initializedPopupColorPicker()
+initializedPopup()
